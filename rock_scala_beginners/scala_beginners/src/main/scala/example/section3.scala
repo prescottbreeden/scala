7312458be4e484afec0112e5a03752fd8aaa4918
prescottbreeden <prescott.breeden@gmail.com>
2021-03-28 15:53:50 -0700

    case classes and map/filter on cons

--- rock_scala_beginners/scala_beginners/src/main/scala/example/section3.scala
+++ rock_scala_beginners/scala_beginners/src/main/scala/example/section3.scala
@@ -314,4 +314,156 @@ object MethodNotations_Excercises {
         else f"${h} ${t.printElements}"
     }
   }
+
+  object AnonymousClasses {
+    abstract class Animal {
+      def eat: Unit
+    }
+
+    val funnyAnimal: Animal = new Animal {
+      override def eat: Unit = println("silly")
+    }
+
+    abstract class Person {
+      val firstName: String
+      val lastName: String
+    }
+
+    val bob = new Person {
+      val firstName = "Bob"
+      val lastName = "Ross"
+    }
+  }
+
+  object More_Exercises {
+    /*
+     * 1. Generic trait MyPredicate[-T] test(T) => boolean
+     * 2. Generic trait MyTransformer[-A, B]
+     * 3. MyList:
+     *  - map(transformer) -> MyList
+     *  - filter(predicate) -> MyList
+     *  - flatMap(transformer from A to MyList[B]) => MyList[B]
+     *
+     *  class EvenPredicate extends MyPredicate[Int]
+     *  class StringToIntTransformer extends MyTransformer[String, Int]
+     *
+     *  [1,2,3].map(double) -> [2, 4, 6]
+     *  [1,2,3,4].filter(even) -> [2,4]
+     *  [1,2,3].flatMap(n -> [n, n+ 1]) => [1,2,2,3,3,4]
+     */
+
+    // crap implementation of filter
+    trait MyPredicate[-T] {
+      def test(elem: T): Boolean
+    }
+    // crap implementation of map
+    trait MyTransformer[-A, B] {
+      def transform(elem: A): B
+    }
+    abstract class MyList[+A] {
+      override def toString(): String = s"[${printElements}]"
+
+      def head: A
+      def tail: MyList[A]
+      def isEmpty: Boolean
+      def printElements: String
+      def add[B >: A](element: B): MyList[B]
+      def map[B](transformer: MyTransformer[A, B]): MyList[B]
+      def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
+      def filter(predicate: MyPredicate[A]): MyList[A]
+      def ++[B >: A](list: MyList[B]): MyList[B]
+    }
+
+    object Empty extends MyList[Nothing] {
+      def printElements: String = ""
+
+      def head: Nothing = throw new NoSuchElementException
+      def tail: Nothing = throw new NoSuchElementException
+      def isEmpty: Boolean = true
+      def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
+      def map[B](transformer: MyTransformer[Nothing,B]): MyList[B] = Empty
+      def flatMap[B](transformer: MyTransformer[Nothing,MyList[B]]): MyList[B] =  Empty
+      def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] =  Empty
+      def ++[B](list: MyList[B]): MyList[B] = list
+    }
+
+    class Cons[+A] (h: A, t: MyList[A]) extends MyList[A] {
+      def printElements: String = 
+        if(t.isEmpty) "" + h
+        else f"${h} ${t.printElements}"
+
+      def head: A = h
+      def tail: MyList[A] = t
+      def isEmpty: Boolean = false
+
+      def add[B >: A](element: B): MyList[B] =
+        new Cons(element, this)
+
+      def map[B](transformer: MyTransformer[A,B]): MyList[B] =
+        new Cons(transformer.transform(h), t.map(transformer))
+
+      def flatMap[B](transformer: MyTransformer[A,MyList[B]]): MyList[B] =
+        transformer.transform(h) ++ t.flatMap(transformer)
+
+      def filter(predicate: MyPredicate[A]): MyList[A] =
+        if (predicate.test(h)) new Cons(h, t.filter(predicate))
+        else t.filter(predicate)
+
+      def ++[B >: A](list: MyList[B]): MyList[B] = 
+        new Cons(h, t ++ list)
+    }
+
+    val list1 = new Cons(1, new Cons(2, new Cons(3, Empty)))
+    val list2 = new Cons(4, new Cons(5, Empty))
+
+    def isOdd(num: Int) = num % 2 != 0
+    def isEven(num: Int) = num % 2 == 0
+    val oddNumbers = new MyPredicate[Int] {
+      def test(elem: Int): Boolean = isOdd(elem)
+    }
+    val evenNumbers = new MyPredicate[Int] {
+      def test(elem: Int): Boolean = isEven(elem)
+    }
+
+    def double(num: Int) = num + num
+    val doubleList = new MyTransformer[Int, Int] {
+      def transform(elem: Int): Int = double(elem)
+    }
+  }
+
+  object CaseClasses {
+
+    case class Person(name: String, age: Int)
+
+    // 1. class parameters are fields
+    val joe = new Person("Joe", 40)
+    println(joe.age)
+    println(joe.name)
+
+    // 2. sensible toString
+    println(joe) // => Person("Joe",40)
+
+    // 3. equals and hashcode implemented OOTB
+    val joe2 = new Person("Joe", 40)
+    println(joe == joe2)
+
+    // 4. case classes have handy copy method
+    val joe3 = joe.copy(age = 100)
+    println(joe3)
+
+    // 5. have companion objects
+    val thePerson = Person
+    val mary = Person("Mary", 32)
+
+    // 6. are serializable
+    // Akka
+
+    // 7. CCs have extractor patterns
+    // can be used in Pattern Matching
+
+    case object UnitedKingdom {
+      def name: String = "The UK of GB and NI"
+    }
+
+  }
 }
